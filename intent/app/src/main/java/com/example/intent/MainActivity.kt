bdaa7af1e22d977940aee0ae5c92aa6640a34b95
Rizqiemn <rizqiesempurna@gmail.com>
2019-10-19 15:04:24 +0700

    Uhuyy

---
+++ intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val id:Int=10
+    val language:String="kotlin"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_klik.setOnClickListener(){
+            intent=Intent(this, ActivityKedua::class.java)
+            intent.putExtra("id_Value",id)
+            intent.putExtra("language_value", language)
+            startActivity(intent)
+        }
+    }
+}
